983733bb4c351a186f80108aa06d2ae888d3064a
jusish <ishimwejustin67@gmail.com>
2024-03-02 10:04:34 +0200

    adding 'link format'

--- src/components/BlogComponent.js
+++ src/components/BlogComponent.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const BlogComponent = ({ img, title, description, program, author, date }) => {
+    const formatTitle = (title) => {
+        return title.toLowerCase().replace(/\s+/g, "-").replace(/[^\w\s-]/g, "");
+      };
   return (
     <div className="flex items-center gap-[30px] h-[100%]">
       <div className="h-full w-[40%]">
@@ -8,7 +12,7 @@ const BlogComponent = ({ img, title, description, program, author, date }) => {
       </div>
       <div className="w-[60%] flex flex-col gap-[20px] py-[4%]">
         <div>
-          <h1 className="text-[#333356] text-[32px]">{title}</h1>
+          <Link to={`/${formatTitle(title)}`} className="text-[#333356] text-[32px]">{title}</Link>
         </div>
         <div>
           <p className="text-[#000] font-bold">{description}</p>
